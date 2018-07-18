d70484fc27d43ca1f0d01aeb915b81dfe3b80ed5
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-17 21:23:43 -0500

    Call 2 asynchronous functions one within the other.  Demonstrate callback hell.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 getUser(1, (user)=>{
     console.log(user);
+    //After getting the user, get the list of repositories user has in his github account.
+    getUserRepos(user, (repos)=>{
+        console.log(`${user.username}'s repos are ${repos}`);
+    });
 });
 
 
@@ -10,5 +14,12 @@ function getUser(id, callback){
         console.log('Reading user id from the database...');
         callback({id: id, username: 'androidbalasu'});
     }, 2000);
-    
-}
+ }
+
+ function getUserRepos(userName, callback){
+     console.log('Get User repos');
+     setTimeout(()=>{
+        console.log('Reading database to get users repos');
+        callback(['repo1','repo2','repo3']);
+     }, 2000);
+ }
