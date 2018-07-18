e2eac491599ded4e2cdab3c17bbd785f5da39ff5
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-18 13:21:02 -0500

    Settimeout function to simulation asynchronous action inside a Promise functor.

--- promise.js
+++ promise.js
@@ -1,9 +1,16 @@
 
+//A promise is an object that will hold the eventual result of an asynchronous operation.
+//It is state is initially set to Pending and will later become reoslved or rejected based on the situation.
 const p = new Promise((resolve, reject)=>{
  //Kick of an async work.
  // ...
-  resolve(1);
+    console.log('Inside Promise functor');
+    setTimeout(()=>{
+        //resolve(1);
+        reject(new Error('Something went wrong'));
+    }, 2000);
+    
 });
 
-
-p.then((result)=> console.log('Result ' + result));
+p.then((result)=> console.log('Result ' + result))
+ .catch(err => console.log('Error ', err.message));
