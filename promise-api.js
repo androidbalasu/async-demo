08b28b6eec3e2c7598e53616c98997bf3e402daf
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-18 15:51:21 -0500

    Returning already resolved promises.   Mostly used in unit testing.

--- promise-api.js
+++ promise-api.js
@@ -5,5 +5,25 @@
 // p.then(result => console.log(result));
 
 //Reject option.
-const p = Promise.reject(new Error('Failed due to unknown problem.'));   //Always use an error object while rejecting promises.
-p.catch(error => console.log(error));
+// const p = Promise.reject(new Error('Failed due to unknown problem.'));   //Always use an error object while rejecting promises.
+// p.catch(error => console.log(error));
+
+//Parallel promises.
+
+const p1 = new Promise((resolve,) =>{
+    setTimeout(()=> {
+        console.log('Async operation 1....');
+        resolve(1);
+    }, 2000)
+});
+
+const p2 = new Promise((resolve) =>{
+    setTimeout(()=> {
+        console.log('Async operation 2....');
+        resolve(2);
+    }, 2000)
+});
+
+
+Promise.all([p1, p2])
+    .then(result => console.log(result));
