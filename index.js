910bfd579d26e758b44c0c8be41975188fc9d169
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-07-17 21:28:48 -0500

    Another asynchronous call to demonstrate 3 async calls and eventually call back hell.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ getUser(1, (user)=>{
     //After getting the user, get the list of repositories user has in his github account.
     getUserRepos(user, (repos)=>{
         console.log(`${user.username}'s repos are ${repos}`);
+        getCommits(repos[0], (commits)=>{
+            console.log(`${repos[0]} has the following commits ${commits}`);
+        });
     });
 });
 
@@ -21,5 +24,13 @@ function getUser(id, callback){
      setTimeout(()=>{
         console.log('Reading database to get users repos');
         callback(['repo1','repo2','repo3']);
+     }, 2000);
+ }
+
+ function getCommits(repo, callback){
+     console.log('Get commits for this repo');
+     setTimeout(()=>{
+         console.log('Get the commits for this database for this repo');
+         callback(['Initial commit', 'Commit 2', 'Commit 3']);
      }, 2000);
  }
